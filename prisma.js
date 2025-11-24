f3e081b6a6c9b332f7927f57d62dec4da5277c3d
Heinz K <heka@techcollege.dk>
2025-11-24 13:18:13 +0100

    Added new models

--- prisma.js
+++ prisma.js
@@ -1,2 +1,4 @@
+// Importerer prisma client
 import { PrismaClient } from '@prisma/client';
+// Exporterer prisma object
 export const prisma = new PrismaClient();
---
+++ prisma/migrations/20251124121731_newmodels/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE `brand` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `logoUrl` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `category` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
